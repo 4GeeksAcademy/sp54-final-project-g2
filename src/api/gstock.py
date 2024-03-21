5fe7f885ab229733c2e89970777ecd8d52adee01
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2024-03-21 21:10:26 +0000

    FEAT: Token Gstock

--- src/api/gstock.py
+++ src/api/gstock.py
@@ -1,53 +1,19 @@
-from flask import request
 
+import requests
+import json
 
-def get_token_access(client_id, client_secret):
-    url_token = 'https://interface-test.g-stock.net/external/api/token'
-    payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'client_credentials'
-    }
+url = os.environ.get('URL_GS')
+client_id = os.environ.get('CLIENT_COB')
+client_secret = os.environ.get('SECRET_COB')
 
-    try:
-        answer = requests.post(url_token, data=payload)
-        if answer.status_code == 200:
-            token_acceso = answer.json().get('access_token')
-            return token_acceso
-        else:
-            print(f"Error al obtener el token de acceso: {answer.status_code}")
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+payload = json.dumps({
+    "client_id": client_id,
+    "client_secret": client_secret
+})
+headers = {
+    'Content-Type': 'application/json'
+}
 
-def get_request_api(url, token_access):
-    headers = {
-        'Authorization': f'Bearer {token_access}',
-        'Content-Type': 'application/json'
-    }
-    try:
-        answer = requests.get(url, headers=headers)
-        if answer.status_code == 200:
-            return answer.json()
-        else:
-            print(f"Error al hacer la solicitud autenticada: {answer.status_code}")
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+response = requests.request("POST", url , headers=headers, data=payload)
 
-# Credenciales de cliente OAuth2
-client_id = '4168-7_MECuFfEInQfI3RvfnSzbPLINZcfbcH2mmzHDFwGKnZJrfG1czC'
-client_secret = 'oUHsYodoVSa51ijaG4aIZCLp9izXvlV3Xsor4ujr6EltJG4aSV'
-
-# Obtener token de acceso
-token = get_token_access(client_id, client_secret)
-if token:
-    # URL del recurso protegido
-    url = 'https://interface-test.g-stock.net/external/api/auth'
-    # Hacer solicitud autenticada
-    datos = get_request_api(url, token)
-    if datos:
-        print("Solicitud autenticada exitosa.")
-        print(datos)
+print(response.text)
