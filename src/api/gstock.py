160b16225faa4cda45e4aaaf07ec3fd57931bb2e
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2024-03-22 10:33:38 +0000

    FEAT: Conexion con Gstock establecida

--- src/api/gstock.py
+++ src/api/gstock.py
@@ -1,19 +1,44 @@
 
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
-url = os.environ.get('URL_GS')
-client_id = os.environ.get('CLIENT_COB')
-client_secret = os.environ.get('SECRET_COB')
 
-payload = json.dumps({
-    "client_id": client_id,
-    "client_secret": client_secret
-})
-headers = {
-    'Content-Type': 'application/json'
-}
+load_dotenv()
+
+# Variables ocultas
+url = os.getenv("URL_GS")
+client_id = os.getenv("CLIENT_COB")
+client_secret = os.getenv("SECRET_COB")
+url_base = os.getenv("URL_GS_BS")
+
+# Obtengo el token
+payload = json.dumps({"client_id": client_id, "client_secret": client_secret})
+headers = {'Content-Type': 'application/json'}
 
 response = requests.request("POST", url , headers=headers, data=payload)
 
-print(response.text)
+# Destructuro la respuesta JSON
+if response.status_code == 200:
+    json_response = response.json()
+    access_token = json_response.get("access_token") # Almaceno el token en una variable
+    center = json_response.get("center")
+    user_centers = json_response.get("userCenters")
+    if user_centers:
+        for center in user_centers:
+            center_id = center.get("id")
+            center_name = center.get("name")
+    if access_token:
+        print("Access Token:", access_token)
+        print("Center:" , center_id, 'Nombre:', center_name )
+    else:
+        print("No se encontró el Token en la respuesta JSON.")
+else:
+    print("Error al enviar la solicitud:", response.status_code)
+
+# Obtener categorias de productos
+headers = {'Authorization': f'Bearer {access_token}'}
+category = requests.get(url_base + 'v1/product/purchases/categories', headers=headers)
+
+print(category.json())
